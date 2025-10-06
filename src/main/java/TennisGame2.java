518d9a6182ade93855338e36e0228f475750da67
Branchlord <Leurat.Zabergja@cas.de>
2025-10-06 14:05:09 +0000

    Gruppe 2

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,9 +1,8 @@
 
-public class TennisGame2 implements TennisGame
-{
+public class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
-    
+
     public String P1res = "";
     public String P2res = "";
     private String player1Name;
@@ -14,115 +13,69 @@ public class TennisGame2 implements TennisGame
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
+    public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4)
-        {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
-        }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
+
+        if (P1point == P2point) {
+            if (P1point <= 4) {
+                score = switch (P1point) {
+                    case 0 -> "Love-All";
+                    case 1 -> "Fifteen-All";
+                    case 2 -> "Thirty-All";
+                    default -> "Deuce";
+                };
+            }
+        } else if (P1point < 4 && P2point < 4) {
+            P1res = getScoreForPlayer(P1point);
+            P2res = getScoreForPlayer(P2point);
+
             score = P1res + "-" + P2res;
+        } else if (P1point >= 4 || P2point >= 4) {
+            int pointdiff = P1point - P2point;
+            if (pointdiff <= -2)
+                score = "Win for player2";
+            else if (pointdiff == -1)
+                score = "Advantage player2";
+            else if (pointdiff == 1)
+                score = "Advantage player1";
+            else
+                score = "Win for player1";
+
         }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point > P2point && P2point >= 3)
-        {
-            score = "Advantage player1";
-        }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2";
-        }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2";
-        }
+
         return score;
     }
-    
-    public void SetP1Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
+
+    private String getScoreForPlayer(int point) {
+        return switch (point) {
+            case 0 -> "Love";
+            case 1 -> "Fifteen";
+            case 2 -> "Thirty";
+            default -> "Forty";
+        };
+    }
+
+    public void SetP1Score(int number) {
+
+        for (int i = 0; i < number; i++) {
             P1Score();
         }
-            
+
     }
-    
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
+
+    public void SetP2Score(int number) {
+
+        for (int i = 0; i < number; i++) {
             P2Score();
         }
-            
+
     }
-    
-    public void P1Score(){
+
+    public void P1Score() {
         P1point++;
     }
-    
-    public void P2Score(){
+
+    public void P2Score() {
         P2point++;
     }
 
