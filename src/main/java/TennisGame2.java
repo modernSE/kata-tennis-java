6f528d7cb9a8d32bb601daf58a9c21c985bb7af4
adriankroeger <adriankroeger@gmx.de>
2025-10-06 13:59:06 +0000

    more refactor

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,8 +1,8 @@
 
 public class TennisGame2 implements TennisGame
 {
-    public int P1point = 0;
-    public int P2point = 0;
+    public int player1Point = 0;
+    public int player2Point = 0;
     
     private String player1Name;
     private String player2Name;
@@ -23,32 +23,32 @@ public class TennisGame2 implements TennisGame
             return "Invalid Score!";
         }
 
-        if (P1point == P2point) {
+        if (player1Point == player2Point) {
             return calculateDrawScore();
         }
         
-        if (P1point != P2point && P1point < GAME_END_SCORE && P2point < GAME_END_SCORE)
+        if (player1Point != player2Point && player1Point < GAME_END_SCORE && player2Point < GAME_END_SCORE)
         {
-            return explicitScores[P1point] + "-" + explicitScores[P2point];
+            return explicitScores[player1Point] + "-" + explicitScores[player2Point];
         }
 
         if (winnerExists()) {
-            return P1point > P2point ? "Win for " + player1Name : "Win for " + player2Name;
+            return player1Point > player2Point ? "Win for " + player1Name : "Win for " + player2Name;
         }
 
-        return P1point > P2point ? "Advantage "  +player1Name : "Advantage " + player2Name;
+        return player1Point > player2Point ? "Advantage "  +player1Name : "Advantage " + player2Name;
     }
 
     private boolean winnerExists(){
-        return (P1point >= GAME_END_SCORE || P2point >= GAME_END_SCORE) && (Math.abs(P1point-P2point) >= GAME_TIEBREAKER_ADVANTAGE);
+        return (player1Point >= GAME_END_SCORE || player2Point >= GAME_END_SCORE) && (Math.abs(player1Point-player2Point) >= GAME_TIEBREAKER_ADVANTAGE);
     }
     
     private void player1Scored(){
-        P1point++;
+        player1Point++;
     }
     
     private void player2Scored(){
-        P2point++;
+        player2Point++;
     }
 
     public void wonPoint(String player) {
@@ -61,15 +61,15 @@ public class TennisGame2 implements TennisGame
     }
 
     private String calculateDrawScore() {
-        if (P1point < 3)
+        if (player1Point < 3)
         {
-            return explicitScores[P1point] + "-All";
+            return explicitScores[player1Point] + "-All";
         }
 
         return "Deuce";
     }
 
         private boolean isInValidScore() {
-            return P1point < 0 || P2point < 0;
+            return player1Point < 0 || player2Point < 0;
     }
 }
